e2dc403b0cb9d7ebcb9df95bc0ca6889d6fa55d7
John <sprague.john@gmail.com>
2020-02-23 12:10:46 +0100

    Python 3 script
    Updated to Python 3. Added Jupyter Notebook

---
+++ README.md
@@ -0,0 +1,10 @@
+# TSP Share Price Scraper
+
+
+Scrapes Thrift Savings Plan (TSP) fund prices from [TSP Website](https://www.tsp.gov/index.html).
+
+
+Adapted for Python 3 from script created by user *Simbills* on the Bogleheads [forum](https://bogleheads.org).
+
+
+See [here](https://www.bogleheads.org/forum/viewtopic.php?f=1&t=108388) for more information.
--- tsp_scrape.py
+++ tsp_scrape.py
@@ -1,64 +1,68 @@
-#!/usr/bin/python
 
-from urllib2 import urlopen
-from urllib import urlencode
+#!/usr/bin/python
+# script to scrape tsp fund prices from tsp.gov
+# adapted from Python 2 script created by user Simbills on Bogleheads.org
+# https://www.bogleheads.org/forum/viewtopic.php?f=1&t=108388
+
+import requests
 import csv
 from datetime import datetime, timedelta, date
-from string import lstrip
+import sys
 
 fundTag = {
-	'L Income' : 'TSPLINCOME',
-	'L 2020' : 'TSPL2020',
-	'L 2030' : 'TSPL2030',
-	'L 2040' : 'TSPL2040',
-	'L 2050' : 'TSPL2050',
-	'G Fund' : 'TSPGFUND',
-	'F Fund' : 'TSPFFUND',
-	'C Fund' : 'TSPCFUND',
-	'S Fund' : 'TSPSFUND',
-	'I Fund' : 'TSPIFUND'}
+    'L Income' : 'TSPLINCOME',
+    'L 2020' : 'TSPL2020',
+    'L 2030' : 'TSPL2030',
+    'L 2040' : 'TSPL2040',
+    'L 2050' : 'TSPL2050',
+    'G Fund' : 'TSPGFUND',
+    'F Fund' : 'TSPFFUND',
+    'C Fund' : 'TSPCFUND',
+    'S Fund' : 'TSPSFUND',
+    'I Fund' : 'TSPIFUND'}
 
 priceHistoryFile = 'tspQuicken.csv'
 
 lastDate = ''
 try:
-	quickenReader = csv.reader(open(priceHistoryFile, 'r'))
-	lastDate = [row for row in quickenReader][-1][2]
+    quickenReader = csv.reader(open(priceHistoryFile, 'r'))
+    lastDate = [row for row in quickenReader][-1][2]
 except:
-	lastDate = '06/01/2003'
-
+    lastDate = '06/01/2003'
 startDate = (datetime.strptime(lastDate, '%m/%d/%Y') + timedelta(1)).strftime('%m/%d/%Y')
 endDate = date.today().strftime('%m/%d/%Y')
 if lastDate == endDate:
-	print 'already have prices through', endDate
-	exit(1)
+    print('already have prices through', endDate)
+    sys.exit()
 
-print 'checking for new prices starting on', startDate
+print('checking for new prices starting on', startDate)
 tspSharePricePageUrl = 'https://www.tsp.gov/InvestmentFunds/FundPerformance/index.html'
-postData = urlencode({'startdate' : startDate, 'enddate' : endDate, 'whichButton' : 'CSV'})
-page = urlopen(tspSharePricePageUrl, postData)
+postData = {'startdate' : startDate, 'enddate' : endDate, 'whichButton' : 'CSV'}
 
-reader = csv.reader(page)
+page = requests.post(tspSharePricePageUrl, data = postData)
+
+reader = csv.reader(page.text.splitlines(), delimiter=',')
 rows = [row for row in reader if len(row) > 0]
 tagRow = rows[0]
 foundNew = False
 
-writer = csv.writer(open(priceHistoryFile, 'a'))
+newRows = []
 for row in rows[:0:-1]:
-	currDate = datetime.strptime(row[0], '%Y-%m-%d').strftime('%m/%d/%Y')
-	newRows = []
-	for i in range(1, len(row)):
-		tag = lstrip(tagRow[i])
-		if tag in fundTag:
-			try:
-				price = float(row[i])
-			except:
-				continue
-			newRows.append([fundTag[tag], price, currDate])
-                        foundNew = True
-			print 'found', [fundTag[tag], price, currDate]
+    currDate = datetime.strptime(row[0], '%Y-%m-%d').strftime('%m/%d/%Y')
+    for i in range(1, len(row)):
+        tag = tagRow[i].lstrip()
+        if tag in fundTag:
+            try:
+                price = float(row[i])
+            except:
+                continue
+            newRows.append([fundTag[tag], price, currDate])
+            foundNew = True
+            print('found', [fundTag[tag], price, currDate])
 
-	writer.writerows(newRows)
+if foundNew:
+    with open(priceHistoryFile, "a", newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(newRows)
 
-if not foundNew:
-	exit(1)
+sys.exit()
