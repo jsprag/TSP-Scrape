ac07ae8d38d08ca36209532762a26e7f476c3ec3
theycallmeroger <372583+theycallmeroger@users.noreply.github.com>
2022-06-20 11:06:21 -0400

    support new TSP.gov website

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # TSP Share Price Scraper
 
+Scrapes Thrift Savings Plan (TSP) fund prices from [TSP Website][tsp].
 
-Scrapes Thrift Savings Plan (TSP) fund prices from [TSP Website](https://www.tsp.gov/index.html).
+# History
+| Date       | Description |
+| ----       | ----------- |
+| 2022-06-03 | [Updated][mysteriousclam-tsp-update] for new TSP.gov website by [mysteriousclam][mysteriousclam-bogle] |
+| 2020-02-03 | [Updated][jsprag-python3] to Python3 by [jsprag][jsprag-bogle] |
+| 2013-01-06 | [Created][simbilis-created] by [Simbilis][simbilis-bogle] on the [Bogleheads forum][bogleheads] |
 
+[tsp]: https://www.tsp.gov
+[bogleheads]: https://bogleheads.org
 
-Adapted for Python 3 from script created by user *Simbilis* on the Bogleheads [forum](https://bogleheads.org).
-
-
-See [here](https://www.bogleheads.org/forum/viewtopic.php?f=1&t=108388) for more information.
+[mysteriousclam-bogle]: https://www.bogleheads.org/forum/memberlist.php?mode=viewprofile&u=186009
+[mysteriousclam-tsp-update]: https://www.bogleheads.org/forum/viewtopic.php?p=6708886#p6708886
+[jsprag-bogle]: https://www.bogleheads.org/forum/memberlist.php?mode=viewprofile&u=127367
+[jsprag-python3]: https://www.bogleheads.org/forum/viewtopic.php?p=5038932#p5038932
+[simbilis-bogle]: https://www.bogleheads.org/forum/memberlist.php?mode=viewprofile&u=40770
+[simbilis-created]: https://www.bogleheads.org/forum/viewtopic.php?f=1&t=108388
--- tsp_scrape.py
+++ tsp_scrape.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python
 # script to scrape tsp fund prices from tsp.gov
 # adapted from Python 2 script created by user Simbilis on Bogleheads.org
@@ -10,21 +9,21 @@ from datetime import datetime, timedelta, date
 import sys
 
 fundTag = {
-    'Linc'  : 'TSPLINCOME',
-    'L2025' : 'TSPL2025',
-    'L2030' : 'TSPL2030',
-    'L2035' : 'TSPL2035',
-    'L2040' : 'TSPL2040',
-    'L2045' : 'TSPL2045',
-    'L2050' : 'TSPL2050',
-    'L2055' : 'TSPL2055',
-    'L2060' : 'TSPL2060',
-    'L2065' : 'TSPL2065',
-    'G'     : 'TSPGFUND',
-    'F'     : 'TSPFFUND',
-    'C'     : 'TSPCFUND',
-    'S'     : 'TSPSFUND',
-    'I'     : 'TSPIFUND'}
+    'L Income'  : 'TSPLINCOME',
+    'L 2025' : 'TSPL2025',
+    'L 2030' : 'TSPL2030',
+    'L 2035' : 'TSPL2035',
+    'L 2040' : 'TSPL2040',
+    'L 2045' : 'TSPL2045',
+    'L 2050' : 'TSPL2050',
+    'L 2055' : 'TSPL2055',
+    'L 2060' : 'TSPL2060',
+    'L 2065' : 'TSPL2065',
+    'G Fund' : 'TSPGFUND',
+    'F Fund' : 'TSPFFUND',
+    'C Fund' : 'TSPCFUND',
+    'S Fund' : 'TSPSFUND',
+    'I Fund' : 'TSPIFUND'}
 
 priceHistoryFile = 'tspQuicken.csv'
 
@@ -33,7 +32,8 @@ try:
     quickenReader = csv.reader(open(priceHistoryFile, 'r'))
     lastDate = [row for row in quickenReader][-1][2]
 except:
-    lastDate = '06/01/2003'
+    lastDate = '01/01/2003'
+
 startDate = (datetime.strptime(lastDate, '%m/%d/%Y') + timedelta(1)).strftime('%Y%m%d')
 endDate = date.today().strftime('%m/%d/%Y')
 if lastDate == endDate:
@@ -41,14 +41,10 @@ if lastDate == endDate:
     sys.exit()
 
 print('checking for new prices starting on', startDate)
-tspSharePricePageUrl = 'https://secure.tsp.gov/components/CORS/getSharePrices.html'
-fundStrings = ['{}=1'.format(fund) for fund in fundTag.keys()]
-dateStrings = ['startdate={}'.format(startDate),
-               'enddate={}'.format(date.today().strftime('%Y%m%d')),
-               'format=CSV', 'download=1']
-restString = '?' +  '&'.join(dateStrings + fundStrings)
+tspSharePricePageUrl = 'https://www.tsp.gov/data/getSharePricesRaw_startdate_20220424_enddate_20220524_Lfunds_1_InvFunds_1_download_0.html'
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36'}
 
-page = requests.get(tspSharePricePageUrl+restString)
+page = requests.get(tspSharePricePageUrl, headers=headers)
 
 reader = csv.reader(page.text.splitlines(), delimiter=',')
 rows = [row for row in reader if len(row) > 0]
@@ -56,18 +52,19 @@ tagRow = rows[0]
 foundNew = False
 
 newRows = []
-for row in rows[:0:-1]:
-    currDate = datetime.strptime(row[0], '%Y-%m-%d').strftime('%m/%d/%Y')
-    for i in range(1, len(row)):
-        tag = tagRow[i].lstrip()
-        if tag in fundTag:
-            try:
-                price = float(row[i])
-            except:
-                continue
-            newRows.append([fundTag[tag], price, currDate])
-            foundNew = True
-            print('found', [fundTag[tag], price, currDate])
+for row in rows[1:]:
+    currDate = datetime.strptime(row[0], '%b %d. %Y').strftime('%m/%d/%Y')
+    if datetime.strptime(currDate, '%m/%d/%Y') >= datetime.strptime(startDate, '%Y%m%d'):
+        for i in range(1, len(row)):
+            tag = tagRow[i].lstrip()
+            if tag in fundTag:
+                try:
+                    price = float(row[i])
+                except:
+                    continue
+                newRows.append([fundTag[tag], price, currDate])
+                foundNew = True
+                print('found', [fundTag[tag], price, currDate])
 
 if foundNew:
     with open(priceHistoryFile, "a", newline='') as file:
